94563462dbf1eb2b4658e1b4b0d86f94c3dc96bd
Bob Kuhar <robertkuhar@gmail.com>
2011-04-23 00:35:02 -0700

    Chapter 3 Exercises

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,37 +3,55 @@ require 'spec_helper'
 describe PagesController do
   integrate_views
 
+  before(:each) do
+    @base_title = 'Ruby on Rails Tutorial Sample App |'
+  end
+
   describe "GET 'home'" do
+    page = 'home'
     it "should be successful" do
-      get 'home'
+      get page
       response.should be_success
-      response.should have_tag('title')
     end
     it "should have correct title" do
-      get 'home'
-      response.should have_tag('title', 'Ruby on Rails Tutorial Sample App | Home')
+      get page
+      response.should have_tag('title', "#{@base_title} #{page.capitalize}")
     end
   end
 
   describe "GET 'contact'" do
+    page = 'contact'
     it "should be successful" do
-      get 'contact'
+      get page
       response.should be_success
     end
     it "should have correct title" do
-      get 'contact'
-      response.should have_tag('title', 'Ruby on Rails Tutorial Sample App | Contact')
+      get page
+      response.should have_tag('title', "#{@base_title} #{page.capitalize}")
     end
   end
 
   describe "GET 'about'" do
+    page = 'about'
     it "should be successful" do
-      get 'about'
+      get page
       response.should be_success
     end
     it "should have correct title" do
-      get 'about'
-      response.should have_tag('title', 'Ruby on Rails Tutorial Sample App | About')
+      get page
+      response.should have_tag('title', "#{@base_title} #{page.capitalize}")
+    end
+  end
+
+  describe "GET 'about'" do
+    page = 'help'
+    it "should be successful" do
+      get page
+      response.should be_success
+    end
+    it "should have correct title" do
+      get page
+      response.should have_tag('title', "#{@base_title} #{page.capitalize}")
     end
   end
 
