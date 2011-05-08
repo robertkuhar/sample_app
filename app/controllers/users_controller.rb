f68ce4d1414d2ddace78cfc5e57d654cdc0134c5
Bob Kuhar <robertkuhar@gmail.com>
2011-05-07 20:40:09 -0700

    Chapter 10 through 10.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
+
   def new
     @user = User.new
     @title = "Sign Up"
@@ -10,7 +13,6 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
     @title = "Edit user"
   end
 
@@ -29,7 +31,6 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
       flash[:success] = "Profile updated."
       redirect_to @user
@@ -37,6 +38,17 @@ class UsersController < ApplicationController
       @title = "Edit user"
       render 'edit'
     end
+  end
+
+  private
+
+  def authenticate
+    deny_access unless signed_in?
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
   end
 
 end
---
+++ spec/integration/friendly_forwardings_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe "FriendlyForwardings" do
+  it "should forward to the requested page after sign" do
+    user = Factory(:user)
+    visit edit_user_path(user)
+    # webrat automatically follows the redirect to the signin page
+    fill_in :email, :with => user.email
+    fill_in :password, :with => user.password
+    click_button
+    # webrat follows the redirect again, this time to users/edit
+    response.should render_template('users/edit')
+  end
+end
